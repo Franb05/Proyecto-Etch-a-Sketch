60cf48087b1906cb67131b8baa6417706fc3565d
Franb05 <146026328+Franb05@users.noreply.github.com>
2026-08-29 17:36:40 -0300

    Generate 16x16 grid with JavaScript

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector("#container");
+
+  for (let fila = 0; fila < 16; fila++) {
+    for (let columna = 0; columna < 16; columna++) {
+      //Creo un cuadrado y lo agrego al contenedor
+      const cuadrado = document.createElement("div");
+      container.appendChild(cuadrado);
+    }
+  }
 });
