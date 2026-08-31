806375121b4820e96a7e70e4feef7b2c9c7ff4f6
Franb05 <146026328+Franb05@users.noreply.github.com>
2026-08-31 18:51:43 -0300

    Make grid size dynamic

--- JS/script.js
+++ JS/script.js
@@ -8,19 +8,22 @@ document.addEventListener("DOMContentLoaded", () => {
     while (resultadoPrompt < 1 || resultadoPrompt > 100) {
       resultadoPrompt = Number(prompt("Introduce un numero entre 1 y 100"));
     }
-  });
 
-  for (let fila = 0; fila < 16; fila++) {
-    for (let columna = 0; columna < 16; columna++) {
-      //Creo un cuadrado y lo agrego al contenedor
-      const cuadrado = document.createElement("div");
-      cuadrado.classList.add("cuadrado");
+    for (let fila = 0; fila < resultadoPrompt; fila++) {
+      for (let columna = 0; columna < resultadoPrompt; columna++) {
+        console.log(resultadoPrompt);
+        //Creo un cuadrado y lo agrego al contenedor
+        const cuadrado = document.createElement("div");
+        cuadrado.style.width = `${640 / resultadoPrompt}px`;
+        cuadrado.style.height = `${640 / resultadoPrompt}px`;
+        cuadrado.classList.add("cuadrado");
 
-      cuadrado.addEventListener("mouseenter", () => {
-        cuadrado.classList.add("pintado");
-      });
+        cuadrado.addEventListener("mouseenter", () => {
+          cuadrado.classList.add("pintado");
+        });
 
-      container.appendChild(cuadrado);
+        container.appendChild(cuadrado);
+      }
     }
-  }
+  });
 });
