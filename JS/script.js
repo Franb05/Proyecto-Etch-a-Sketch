b0b2a98c76d771a1b843e3bd077307ef291edc48
Franb05 <146026328+Franb05@users.noreply.github.com>
2026-08-30 21:18:00 -0300

    Add grid size input validation

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector("#container");
 
+  const btnCuadrados = document.querySelector("#btn-cuadrados");
+  btnCuadrados.addEventListener("click", () => {
+    let resultadoPrompt = Number(prompt("Introduce un numero de casillas"));
+
+    while (resultadoPrompt < 1 || resultadoPrompt > 100) {
+      resultadoPrompt = Number(prompt("Introduce un numero entre 1 y 100"));
+    }
+  });
+
   for (let fila = 0; fila < 16; fila++) {
     for (let columna = 0; columna < 16; columna++) {
       //Creo un cuadrado y lo agrego al contenedor
